d30cf1b165d1fe88df5d0e6e5a32ccd61357cce2
Shashi Bhushan Yadav <98235478+shashibyadav@users.noreply.github.com>
2024-05-13 20:40:45 -0400

    completing verification

--- services/textract/Textract.py
+++ services/textract/Textract.py
@@ -18,5 +18,19 @@ class Textract:
         )
         return extracted_text
 
+    def get_multi_analyze_id(self, file_names=[]):
+        request = []
+        for file_name in file_names:
+            request.append({
+                "S3Object": {
+                    "Bucket": config_obj.get_bucket_name(),
+                    "Name": file_name
+                }
+            })
+        extracted_text = self._client.analyze_id(
+            DocumentPages=request
+        )
+        return extracted_text
+
 
 textract_client = Textract()
